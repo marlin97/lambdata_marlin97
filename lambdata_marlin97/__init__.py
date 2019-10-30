901a159e80de1921e127d81cba0ba094db3639a1
marlin97 <vandamm97@gmail.com>
2019-10-29 23:48:06 -0400

    create virt env; add helper funcs

---
+++ lambdata_marlin97/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.0.1"
+some_str = "This is a starting to test __init.py"
---
+++ lambdata_marlin97/df_utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+
+ones = pd.Series(np.ones(20))
+zeros = pd.Series(np.zeros(20))
+
+
