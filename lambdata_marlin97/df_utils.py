62285f73399ca434c9f796bdc84491b3d6c3b743
marlin97 <vandamm97@gmail.com>
2019-10-30 21:34:34 -0400

    Added nan checker

--- lambdata_marlin97/df_utils.py
+++ lambdata_marlin97/df_utils.py
@@ -5,4 +5,8 @@ import pandas as pd
 ones = pd.Series(np.ones(20))
 zeros = pd.Series(np.zeros(20))
 
+def check_nulls(df):
+    nans = sum(df.isna().sum())
+    print("This DataFrame contains {} missing values".format(nans))
+
 
