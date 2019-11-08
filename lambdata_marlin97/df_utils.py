3ec6106709a48608ff1087045aa43db051c9d779
Ross Glasmann <vandamm97@gmail.com>
2019-11-07 21:46:48 -0500

    Update

--- lambdata_marlin97/df_utils.py
+++ lambdata_marlin97/df_utils.py
@@ -11,10 +11,17 @@ class LambData:
         self.df = df
 
     def check_nulls(self):
+        """
+        Checks dataframe for Null values
+        """
         nans = sum(self.df.isna().sum())
         print("This DataFrame contains {} missing values".format(nans))
 
     def list_to_col(self, arry, col_name):
+        """
+        Takes a list, then converts it to a Series object, and add it to the dataframe as a column
+        :returns: Dataframe with new column
+        """
         col_name = str(col_name) #ensure col name is a string
         series_obj = pd.Series(arry)
         self.df[col_name] = series_obj
